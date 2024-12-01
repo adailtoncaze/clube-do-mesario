fbe7c7e5b860a80ef984b42c1146d890a66cd182
adailtoncaze <adailtoncaze@gmail.com>
2024-12-01 15:13:03 -0300

    Update project

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: [], // adicione aqui os domínios externos de imagens se necessário
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.join(__dirname, 'src'),
+    }
+    return config
   },
 }
 
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,18 @@
-import Header from '@/components/Header'
-import Hero from '@/components/Hero'
-import Highlights from '@/components/Highlights'
-import AboutProject from '@/components/AboutProject'
-import Partners from '@/components/Partners'
-import Benefits from '@/components/Benefits'
-import Footer from '@/components/Footer'
+import Header from '../components/Header'
+import Hero from '../components/Hero'
+import Categories from '../components/Categories'
+import Highlights from '../components/Highlights'
+import AboutProject from '../components/AboutProject'
+import Partners from '../components/Partners'
+import Benefits from '../components/Benefits'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <Header />
       <Hero />
+      <Categories />
       <Highlights />
       <AboutProject />
       <Partners />
