c9268b577b7ea9c46ddab064f224d3dcfb787e0a
adailtoncaze <adailtoncaze@gmail.com>
2024-12-01 15:16:12 -0300

    Update project

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,5 @@
 import Header from '../components/Header'
 import Hero from '../components/Hero'
-import Categories from '../components/Categories'
 import Highlights from '../components/Highlights'
 import AboutProject from '../components/AboutProject'
 import Partners from '../components/Partners'
@@ -12,7 +11,6 @@ export default function Home() {
     <main className="min-h-screen">
       <Header />
       <Hero />
-      <Categories />
       <Highlights />
       <AboutProject />
       <Partners />
